a13a09add3359a8597c530cc166718c6fa27a287
Mesquita <mesquita@aospa.co>
2023-02-14 18:46:52 -0300

    MainActivity: Add CountUp button
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- java/co/mesquita/diceroller/MainActivity.kt
+++ java/co/mesquita/diceroller/MainActivity.kt
@@ -12,11 +12,23 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+
+        val countUpButton: Button = findViewById(R.id.count_up_button)
+        countUpButton.setOnClickListener { countUp() }
     }
 
     private fun rollDice() {
         val randomInt = (1..6).random()
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
+    }
+
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val newValue = resultText.text.toString().toIntOrNull() ?: 0
+
+        if (newValue != 6) {
+            resultText.text = (newValue+1).toString()
+        }
     }
 }

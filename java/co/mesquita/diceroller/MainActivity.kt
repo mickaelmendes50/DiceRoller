ec0a713c5aa33a6c442eb2905eb8f3e43740fa88
Mesquita <mesquita@aospa.co>
2023-02-15 15:42:31 -0300

    DiceRolle: drop count up function
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- java/co/mesquita/diceroller/MainActivity.kt
+++ java/co/mesquita/diceroller/MainActivity.kt
@@ -6,41 +6,38 @@ import android.widget.Button
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var diceImage : ImageView
+    lateinit var leftDiceImage : ImageView
+    lateinit var rightDiceImage : ImageView
     private var randomInt = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        diceImage = findViewById(R.id.dice_image)
+        leftDiceImage = findViewById(R.id.dice_image_left)
+        rightDiceImage = findViewById(R.id.dice_image_right)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener { rollDice() }
-
-        val countUpButton: Button = findViewById(R.id.count_up_button)
-        countUpButton.setOnClickListener { countUp() }
+        rollButton.setOnClickListener {
+            rollDice(leftDiceImage)
+            rollDice(rightDiceImage)
+        }
 
         val resetButton: Button = findViewById(R.id.reset_button)
         resetButton.setOnClickListener { reset() }
     }
 
-    private fun rollDice() {
+    private fun rollDice(imageView: ImageView) {
         randomInt = (1..6).random()
-        updateDiceImage(randomInt)
-    }
-
-    private fun countUp() {
-        if (randomInt != 6) {
-            updateDiceImage(++randomInt)
-        }
+        updateDiceImage(randomInt, imageView)
     }
 
     private fun reset() {
-        diceImage.setImageResource(R.drawable.empty_dice)
+        leftDiceImage.setImageResource(R.drawable.empty_dice)
+        rightDiceImage.setImageResource(R.drawable.empty_dice)
         randomInt = 0
     }
 
-    private fun updateDiceImage(randomInt: Int) {
+    private fun updateDiceImage(randomInt: Int, imageView: ImageView) {
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -50,6 +47,6 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-        diceImage.setImageResource(drawableResource)
+        imageView.setImageResource(drawableResource)
     }
 }
